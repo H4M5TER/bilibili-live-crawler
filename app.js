ccb497a7d2c70a2ba5fa1fbcf62106d2951cef48
H4M5TER <zhuxx2003@163.com>
2020-02-12 05:20:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const axios = require("axios");
 const fs = require("fs");
-const getCookie = require("./cookie").getCookie;
-const store = require("./database").store;
+const store = require("./db").store;
 
+//读取配置文件
 fs.readFile("config.json", "utf8", (err, data) => {
+	//错误处理
 	if (err) {
+		//检测初始化
 		if (err.code === "ENOENT") {
 			console.log("config.json doesn't exist.\nIt will be created later.\nPlease edit it and start the crawler again.");
 			const config = {
-				"streamer": [
-					{
-						"name": "name",
-						"uid": 123456
-					}
+				"uid": [
+					123456
 				]
 			};
-			try {
-				config.cookie = getCookie();
-			} catch (err) {
-				console.error("Cookie can't fetch: " + err.toString());
-			}
 			fs.writeFile("config.json", JSON.stringify(config, null, "\t"), "utf8", (err) => {
 				if (err) {
 					console.error(err);
@@ -28,27 +22,19 @@ fs.readFile("config.json", "utf8", (err, data) => {
 			})
 		}
 		else {
-			return console.error(err);
+			console.error(err);
 		}
+		return;
 	}
 	else
 		const config = JSON.parse(data);
-
-	{
-		let remains = (new Date()).getTime() - (new Date(config.cookie.expires)).getTime();
-		setTimeout(() => {
-			config.cookie = getCookie();
-			setInterval(() => {
-				config.cookie = getCookie();
-			}, 2592000000 - 6000);
-		}, remains < 6000 ? 0 : remains - 6000)
-	}
-
+	// monitoring用于记录uid是否监视
 	let monitoring = new Map();
-	config.streamer.forEach((streamer) => {
-		monitoring.set(streamer.uid, false);
-	});
-	let checkOnline = setInterval(() => {
+	for (let i of config.uid)
+		monitoring.set(i, false);
+	// 定时检查监视用户是否开播 1分钟一次
+	setInterval(() => {
+		// 如果全部已经开播 跳过本次检查
 		let flag = true;
 		for (let i of monitoring) {
 			if (i[1] === false) {
@@ -58,53 +44,45 @@ fs.readFile("config.json", "utf8", (err, data) => {
 		}
 		if (flag)
 			return;
-		axios.request({
-			url: "https://vup.darkflame.ga/api/online",
-			method: "GET",
-			header: {
-				"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.131 Safari/537.36",
-				"cookie": config.cookie.value;
-			}
-		}).then((response) => {
-			return response.json();
+		// 获取直播列表 检查监视用户是否开播
+		axios.get("https://vup.darkflame.ga/api/online").then((response) => {
+			return response.data; //axios的response和原生的response接口不同
 		}).then((data) => {
 			data.list.forEach((streamer) => {
-				if (monitoring.has(streamer.uid) && !monitoring.get(streamer.uid)) {
-					let connectionToken;
-					axios.request({
-						url: "https://vup.darkflame.ga/roomHub/negotiate?roomId=21752710&negotiateVersion=1",
-						method: "POST",
-						header: {
-							"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.131 Safari/537.36",
-							"cookie": config.cookie.value;
+				// 如果开播
+				if (monitoring.has(streamer.uid) && !monitoring.get(streamer.uid)) { //TODO: 或许有更优雅的写法
+					// 获取网站要求的token 连接websocket
+					axios.post("https://vup.darkflame.ga/roomHub/negotiate?roomId=" + streamer.uid.toString() + "&negotiateVersion=1").then((response) => {
+						let ws = new WebSocket("wss://vup.darkflame.ga/roomHub?roomId=" + streamer.roomId.toString() + "&id=" + response.data.connectionToken);
+						ws.onopen = (event) => {
+							ws.send('{"protocol":"json","version":1}\u001e');
+							// websocket连接时 把当前uid记为已经监视
+							monitoring.set(streamer.uid, true);
+							// 开始发送网站应用层的心跳包 15秒一次
+							setInterval(() => {
+								ws.send('{"type":6}\u001e');
+							}, 15 * 60 * 1000);
 						}
-					}).then((response) => {
-						return response.json();
-					}).then((data) => {
-						connectionToken = data.connectionToken;
+						ws.onmessage = (event) => {
+							// 拆开合并的数据包
+							event.data.split("\u001e").forEach((data) => {
+								let data = JSON.parse(data);
+								// 排除心跳包和空包
+								if (data.type !== 1)
+									return;
+								// 交给与数据库交互的部分处理
+								store(data);
+							})
+						}
+						ws.onclose = (event) => {
+							// websocket关闭时 把当前uid记为停止监视
+							monitoring.set(streamer.uid, false);
+						}
 					}).catch((err) => {
-						console.error("Get websocket token failed.")
+						//错误处理
+						console.error("Open websocket failed.")
 						return console.error(err);
 					})
-					let ws = new WebSocket("wss://vup.darkflame.ga/roomHub?roomId=" + streamer.roomId.toString() + "&id=" + connectionToken);
-					ws.onopen = (event) => {
-						ws.send('{"protocol":"json","version":1}\u001e');
-						monitoring.set(streamer.uid, true);
-						setInterval(() => {
-							ws.send('{"type":6}\u001e');
-						}, 15 * 60 * 1000);
-					}
-					ws.onmessage = (event) => {
-						event.data.split("\u001e").forEach((data) => {
-							let _data = JSON.parse(data);
-							if (_data.type !== 1)
-								return;
-							store(_data);
-						})
-					}
-					ws.onclose = (event) => {
-						monitoring.set(streamer.uid, false);
-					}
 				}
 			})
 		}).catch((err) => {
